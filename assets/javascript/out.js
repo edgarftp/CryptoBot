8f4cbac965dd78a1734721b28549c0c2865e4d94
Develanton <develanton@gmail.com>
2018-06-06 01:33:42 -0500

    added logout file

---
+++ assets/javascript/out.js
@@ -0,0 +1,12 @@
+
+    const auth = firebase.auth();
+
+    const btnLogOut = document.getElementById('btnLogOut');
+
+    btnLogOut.addEventListener('click', e => {
+        e.preventDefault();
+
+        firebase.auth().signOut();
+
+    });
+
