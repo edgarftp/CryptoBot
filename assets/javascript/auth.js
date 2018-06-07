925e522ea2d15d73898b3f2705bfb67bcf12a53b
Develanton <develanton@gmail.com>
2018-06-07 18:14:15 -0500

    carrusel

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -37,7 +37,7 @@
                 // comment unused sign-in method
                 firebase.auth.GoogleAuthProvider.PROVIDER_ID,
                 firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+                //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
                 // firebase.auth.GithubAuthProvider.PROVIDER_ID,
                 firebase.auth.EmailAuthProvider.PROVIDER_ID
             ],
